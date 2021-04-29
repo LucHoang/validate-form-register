d63cc57e0f96c99ae0710e305c5f9a80fd7871bc
LucHoang <hoangsyluc@gmail.com>
2021-04-29 15:43:56 +0700

    Validate form đăng ký

---
+++ src/main/java/com/hsl/model/User.java
@@ -0,0 +1,97 @@
+package com.hsl.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class User implements Validator {
+    @Size(min = 5, max = 45)
+    private String firstName;
+    @Size(min = 5, max = 45)
+    private String lastName;
+//    @Pattern(regexp="(84|0[3|5|7|8|9])+([0-9]{8})\\b", message = "phone number is not in the correct format")
+    private String phoneNumber;
+    @Min(18)
+    private int age;
+    @Email
+    private String email;
+
+    public User() {
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        User user = (User) target;
+        String phoneNumber = user.getPhoneNumber();
+        ValidationUtils.rejectIfEmpty(errors, "phoneNumber", "phoneNumber.empty");
+        if (phoneNumber.length()>11 || phoneNumber.length()<10){
+            errors.rejectValue("phoneNumber", "phoneNumber.length");
+        }
+        if (!phoneNumber.startsWith("0")){
+            errors.rejectValue("phoneNumber", "phoneNumber.startsWith");
+        }
+        if (!phoneNumber.matches("(^$|[0-9]*$)")){
+            errors.rejectValue("phoneNumber", "phoneNumber.matches");
+        }
+    }
+
+    public User(String firstName, String lastName, String phoneNumber, int age, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
